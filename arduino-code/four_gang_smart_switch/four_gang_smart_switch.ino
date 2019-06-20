4dca87219dbcaa8f878e57aa26676206324d97b5
Brandon Wood <malceore@gmail.com>
2019-06-19 20:22:24 -0400

    Migrating to monorepo version to track all my files across multiple devices in one place.

---
+++ arduino-code/four_gang_smart_switch/four_gang_smart_switch.ino
@@ -0,0 +1,144 @@
+/**
+ * Simple server compliant with Mozilla's proposed WoT API
+ * Originally based on the HelloServer example
+ * Tested on ESP8266, ESP32, Arduino boards with WINC1500 modules (shields or
+ * MKR1000)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <Arduino.h>
+#include "Thing.h"
+#include "WebThingAdapter.h"
+const char* ssid = "GOAT";
+const char* password = "goatseatgrass";
+const int ledPin = LED_BUILTIN;
+WebThingAdapter* adapter;
+
+const int pin1=16; 
+const int pin2=5;
+const int pin3=4; 
+const int pin4=0;
+
+const char* ledTypes[] = {"OnOffSwitch", "Light", nullptr};
+ThingDevice led1("led1", "Built-in LED", ledTypes);
+ThingDevice led2("led2", "Built-in LED", ledTypes);
+ThingDevice led3("led3", "Built-in LED", ledTypes);
+ThingDevice led4("led4", "Built-in LED", ledTypes);
+
+ThingProperty led1On("on", "", BOOLEAN, "OnOffProperty");
+ThingProperty led2On("on", "", BOOLEAN, "OnOffProperty");
+ThingProperty led3On("on", "", BOOLEAN, "OnOffProperty");
+ThingProperty led4On("on", "", BOOLEAN, "OnOffProperty");
+
+bool lastOn1 = false;
+bool lastOn2 = false;
+bool lastOn3 = false;
+bool lastOn4 = false;
+
+void setup(void){
+  
+  pinMode(pin1, OUTPUT); 
+  digitalWrite(pin1, LOW);
+  pinMode(pin2, OUTPUT);
+  digitalWrite(pin2, LOW);
+  pinMode(pin3, OUTPUT);
+  digitalWrite(pin3, LOW);
+  pinMode(pin4, OUTPUT);
+  digitalWrite(pin4, LOW);
+  
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.print("Connecting to \"");
+  Serial.print(ssid);
+  Serial.println("\"");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  bool blink = true;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(ledPin, blink ? LOW : HIGH); // active low led
+    blink = !blink;
+  }
+  digitalWrite(ledPin, HIGH); // active low led
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  adapter = new WebThingAdapter("w25", WiFi.localIP());
+
+  led1.addProperty(&led1On);
+  led2.addProperty(&led2On);
+  led3.addProperty(&led3On);
+  led4.addProperty(&led4On);
+  
+  adapter->addDevice(&led1);
+  adapter->addDevice(&led2);
+  adapter->addDevice(&led3);
+  adapter->addDevice(&led4);
+  adapter->begin();
+  
+  Serial.println("HTTP server started");
+
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/things/");
+  Serial.println(led1.id);
+  
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/things/");  
+  Serial.println(led2.id);
+
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/things/");
+  Serial.println(led3.id);
+  
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/things/");
+  Serial.println(led4.id);
+}
+
+void loop(void){
+  adapter->update();
+  bool on1 = led1On.getValue().boolean;
+  bool on2 = led2On.getValue().boolean;
+  bool on3 = led3On.getValue().boolean;
+  bool on4 = led4On.getValue().boolean;
+  
+  if (on1 != lastOn1 || on2 != lastOn2 || on3 != lastOn3 || on4 != lastOn4) {
+    digitalWrite(pin1, on1 ? LOW : HIGH); // active low led
+    Serial.print(led1.id);
+    Serial.print(": ");
+    Serial.println(on1);
+
+    digitalWrite(pin2, on2 ? LOW : HIGH); // active low led
+    Serial.print(led2.id);
+    Serial.print(": ");
+    Serial.println(on2);
+
+    digitalWrite(pin3, on3 ? LOW : HIGH); // active low led
+    Serial.print(led3.id);  
+    Serial.print(": ");
+    Serial.println(on3);
+
+    digitalWrite(pin4, on4 ? LOW : HIGH); // active low led
+    Serial.print(led4.id);
+    Serial.print(": ");
+    Serial.println(on4);
+  } 
+  lastOn1 = on1;
+  lastOn2 = on2;
+  lastOn3 = on3;
+  lastOn4 = on4;
+}
